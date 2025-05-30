2ec8c2790ee19626ba7b1269e43716c00979895e
Cheikhouna FALL <cheikhounafall20012@gmail.com>
2025-05-30 07:45:24 +0000

    Fix: replace bcrypt with bcryptjs import in auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,7 @@
  */
 
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const logger = require('../utils/logger');
 const { supabase } = require('../database/supabase');
 
